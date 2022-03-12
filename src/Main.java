3c7417aa25feb1b37d3da1f13e7c744199063615
Kozimjon <kozimkuchkarov77@gmail.com>
2022-03-11 19:30:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import com.sun.media.jfxmediaimpl.HostUtils;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+     for (int row = 0; row <= 5; row++)
+     {
+         for (int col = 0; col <= 5; col++)
+         {
+             System.out.println("*");
+            }
+        }
+    }
+}
